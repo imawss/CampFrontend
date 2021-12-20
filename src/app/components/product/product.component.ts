ed81c7ecbbaf1b156269d2fc736b4e9192802dde
Aras Arkan <83475962+ArasAdam@users.noreply.github.com>
2021-12-20 14:01:31 +0300

    20 aralık commit 2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/modals/product';
+import {HttpClient} from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/modals/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -10,10 +12,18 @@ export class ProductComponent implements OnInit {
 
 
   products: Product[] = [];
+  apiUrl ="https://localhost:44396/api/products/getall"
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
+      this.products = response.data
+    });
   }
 
 }
